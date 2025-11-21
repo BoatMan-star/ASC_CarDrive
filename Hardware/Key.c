dc42b9427ab3e2064900b20b55c8447f0822f67f
Boatman <504470639@qq.com>
2025-11-21 14:42:29 +0800

    final_no debug

---
+++ Hardware/Key.c
@@ -0,0 +1,53 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+
+
+void Key_Init(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStructure_A;
+	GPIO_InitStructure_A.GPIO_Mode=GPIO_Mode_IPU;
+	GPIO_InitStructure_A.GPIO_Pin=GPIO_Pin_0|GPIO_Pin_2;
+	GPIO_InitStructure_A.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure_A);
+
+}
+
+uint8_t Key_GetNum(void)
+{
+	static uint8_t KeyNum = 1;
+	static uint8_t Time_Key;
+	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0)
+	{
+		Time_Key++;
+		if (Time_Key>=2) //防止震动间隔20ms后判断是否还在按下状态
+		{
+		Time_Key=0;
+		while (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0);  //等待松手
+		KeyNum = !KeyNum;
+		}
+	}
+	
+	return KeyNum;
+}
+
+uint16_t Key_Speed(void)
+{
+	static uint16_t KeySpeed = 0;
+	static uint8_t Time_Key;
+	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0)
+	{
+		Time_Key++;
+		if (Time_Key>=2) //防止震动间隔20ms后判断是否还在按下状态
+		{
+		Time_Key=0;
+		while (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0);  //等待松手
+		KeySpeed+=30;
+		if (KeySpeed>=100) KeySpeed=0;
+		}
+	}
+	
+	return KeySpeed;
+}
+
---
+++ User/main.c
@@ -0,0 +1,114 @@
+#include "stm32f10x.h"                  // Device header
+#include "Encoder.h"
+#include "PWM.h"
+#include "Moter.h"
+#include "Key.h"
+#include "Serial.h"
+#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "Timer.h"
+#include "OLED.h"
+#include "PID.h"
+#include "Sensor.h"
+#include "Image.h"
+
+#define Stringht     1
+#define LightLeft    2
+#define LightRight   3
+#define StrLeft      4
+#define StrRight     5
+
+uint8_t Key_Num;   //小车启动按键
+
+uint8_t Route_Flag;  //小车路线判断
+
+//电机旋转速度的实际值
+int16_t Speed1;
+int16_t Speed2;
+
+//PID控制启动标志变量
+uint8_t PIDControl;
+
+
+//电机目标速度
+int16_t Speed_Target;
+
+
+//提取接收到的数据包中的数字（速度）
+int16_t ExtractSpeed(void);
+
+int main(void)
+{
+	
+	Moter_Init();
+	Key_Init();
+	Time_Init();
+  Encoder_Init();
+	Serial_Init();
+	OLED_Init();
+	Sensor_Init();
+	
+	while (1)
+	{
+		Image_Control();    //实现菜单调速功能
+	}
+		
+}
+
+
+void TIM2_IRQHandler(void)
+{
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
+	{
+		static int16_t cnt;
+		cnt++;
+		
+		if (cnt>=10)
+		{
+		
+			cnt=0;
+			//每10ms读取一次电机旋转速度实际值
+		
+			Speed1=Encoder_GetSpeed1();
+		
+			Speed2=Encoder_GetSpeed2();
+		
+		
+			Key_Num=Key_GetNum();   //监视按键状态
+		
+		
+			Route_Flag=Route_Judge();
+			
+		
+			Speed_Target=Key_Speed();
+			
+		
+		if (Key_Num==0) //按下按键小车启动
+    {
+		if (Route_Flag==Stringht)
+		{
+			if (PIDControl>=1) PID_Straight();
+		}
+		else if (Route_Flag==LightLeft)
+		{
+			if (PIDControl>=1) PID_LeftSlight();
+		}
+		else if (Route_Flag==LightRight)
+		{
+			if (PIDControl>=1) PID_RightSlight();
+		}
+		else if (Route_Flag==StrLeft)
+		{
+			if (PIDControl>=1) PID_LeftStraight();
+		}
+		else if (Route_Flag==StrRight)
+		{
+			if (PIDControl>=1) PID_RightStraight();
+		}
+	  }
+	
+		}
+		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+	}
+}
